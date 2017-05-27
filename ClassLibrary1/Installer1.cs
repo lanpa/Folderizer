b3ebe31f139209b25a9ad68a55b282da97792bb8
Tzu-Wei Huang <huang.dexter@gmail.com>
2017-05-27 12:19:23 +0800

    add setup project
    This release removes support for .net 3.5 and changes the installation
    into windows installer.

---
+++ ClassLibrary1/Installer1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ClassLibrary1
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(System.Collections.IDictionary savedState)
+        {
+            base.Commit(savedState);
+            // Get the location of regasm
+            string regasmPath = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory() + @"regasm.exe";
+            if (System.Environment.Is64BitOperatingSystem) //to prevent wow6432 issue
+            {
+                regasmPath = regasmPath.Replace("Framework", "Framework64");
+            }
+            //
+            // Get the location of our DLL
+            string componentPath = typeof(Installer1).Assembly.Location;
+            //MessageBox.Show(regasmPath + "\"" + componentPath + "\"" + " /codebase");
+            System.Threading.Thread.Sleep(1000);
+            // assert file is copied
+            System.Diagnostics.ProcessStartInfo processInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+            processInfo.Verb = "runas";
+            processInfo.Arguments = "/K "+ regasmPath + " /codebase" + " \"" + componentPath + "\"";
+            //System.Diagnostics.Process.Start(processInfo);
+            // Execute regasm
+            System.Diagnostics.Process.Start(regasmPath, "\"" + componentPath +"\"" + " /codebase");
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        //public override void Uninstall(System.Collections.IDictionary savedState)
+        protected override void OnBeforeUninstall(System.Collections.IDictionary savedState)
+        {
+            base.OnBeforeUninstall(savedState);
+            string regasmPath = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory() + @"regasm.exe";
+            if (System.Environment.Is64BitOperatingSystem) //to prevent wow6432 issue
+            {
+                regasmPath = regasmPath.Replace("Framework", "Framework64");
+            }
+            string componentPath = typeof(Installer1).Assembly.Location;
+            //MessageBox.Show(regasmPath + "\"" + componentPath + "\"" + " /codebase");
+
+            System.Diagnostics.ProcessStartInfo processInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+            processInfo.Verb = "runas";
+            processInfo.Arguments = "/K "+ regasmPath + " /unregister \"" + componentPath + "\"" + " /codebase";
+            //System.Diagnostics.Process.Start(processInfo);
+            System.Diagnostics.Process.Start(regasmPath, "\"" + componentPath + "\"" + " /codebase /unregister");
+
+            System.Threading.Thread.Sleep(1000);
+        }
+
+
+          
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+
+        }
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -5,12 +5,5 @@ A shell extension that moves selected files into a new folder.
 Microsoft windows 10, windows 8, windows 7
 
 # Install
-1. Download and unzip [installFolderizer.zip](http://oz.nthu.edu.tw/~u932014/installFolderizer.zip).
-2. Depending on your OS version, right click on either *install_win7[.bat]* or *install_win8_win10[.bat]*; in the context menu, click *run as administrator*.
-3. Enjoy.
-
-
-# Uninstall
-1. Download and unzip [installFolderizer.zip](http://oz.nthu.edu.tw/~u932014/installFolderizer.zip).
-2. Depending on your OS version, right click on either *uninstall_win7[.bat]* or *uninstall_win8_win10[.bat]*; in the context menu, click *run as administrator*.
-3. Bye  T.T 
+1. Download and unzip [installFolderizer.zip].
+2. Run setup.exe (It will install missing .Net framework if needed.)
